2120413ca65bca01c23953e55de017377ac8006a
Lyle Patterson <lyle.patterson2002@gmail.com>
2023-12-31 15:40:16 +0000

    Made title screen and changed projectile deletion

--- Assets/Main Scene Components/Scripts/Other/ProjectileManager.cs
+++ Assets/Main Scene Components/Scripts/Other/ProjectileManager.cs
@@ -5,14 +5,21 @@ public class ProjectileManager : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
-        //if (other.gameObject.CompareTag("Enemy")) 
-        //{
-        //    Destroy(gameObject);    // Make so destroys once hit anything, not specifically enemy               
-        //}
+        if (other.gameObject.CompareTag("Enemy") && gameObject.CompareTag("Bullet"))
+        {
+            Destroy(gameObject);    // Make so destroys once hit anything, not specifically enemy               
+        }
 
-        // Destroy(gameObject);
+        if(other.gameObject.CompareTag("Player") && gameObject.CompareTag("Rock"))
+        {
+            Destroy(gameObject);
+        }
+        // Have code for just destroying object in general?
     }
-
+    private void OnCollisionEnter(Collision collision)
+    {
+        Destroy(gameObject);
+    }
     public IEnumerator deleteProjectile(GameObject obj)
     {
         yield return new WaitForSeconds(2f);
